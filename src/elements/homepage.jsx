66b6dc1f067aa207aa41b6605fa022903957ec9c
Nataniel Sitarz <natsitarz@icloud.com>
2024-09-04 16:11:26 +0200

    style: Update homepage content alignment and add logo

--- src/elements/homepage.jsx
+++ src/elements/homepage.jsx
@@ -1,13 +1,18 @@
 function Homepage() {
   return (
     <div className="homePageContent">
-      <div className="leftContent">
+      <div className="centerContent">
         <h1>Znicze? Tylko u nas!</h1>
         <p>
           Jesteśmy sklepem który zajmuje się handlem oraz sprzedażą zniczy.
-          Sprzedajemy duże ilości hurtowo, a także prowadzimy sprzedaż
+          <br /> Sprzedajemy duże ilości hurtowo, a także prowadzimy sprzedaż
           detaliczną.
         </p>
+        <img
+          class="longLogo"
+          src="./long-logo.png"
+          alt="Długie logo strony głównej"
+        />
       </div>
     </div>
   );
